0ac52757bf14cb1e9c3203a20f861baec347d864
Fatimazahraabboud <58141716+Fatimazahraabboud@users.noreply.github.com>
2019-11-24 17:29:59 +0100

    Update grille.cpp

--- grille.cpp
+++ grille.cpp
@@ -7,7 +7,7 @@
 }*/
 
 void Grille::SetLongeur(int x){
-D_Longeur=x;
+D_Longeur=x
 }
 
 int Grille::GetLargeur()const{
@@ -23,19 +23,6 @@ return D_Longeur;
 /*Grille::afficheGrille()const{
 
 
-for (int i(0); i < 5; i++) {
-		for (int j(0); j < 5; j++) {
-			if (grille[i][j] == '_' && i == 4)
-				cout << " ";
-			else
-				cout << grille[i][j];
-			if (j != 4)
-				cout << "|";
-			else
-				cout << endl;
-		}
-	}
-	cout << endl;
 
 
 }*/
